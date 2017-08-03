b2e2ee38923460a3fd72be475720629d76064618
JorgeRZT <georgeforceone@gmail.com>
2017-08-03 13:44:30 +0200

    Agregar archivos de proyecto.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Configuration
+    {
+        private static Configuration _instance;
+        //Constructor privado para evitar que alguien cree otra instancia de la clase
+        private Configuration()
+        {
+            //Atributos
+        }
+
+        //El getter crea la insrtancia si no existe, finalmente se devuelve la instancia
+        public static Configuration Instance
+        {
+            get
+            {
+                if(null == _instance)
+                {
+                    _instance = new Configuration();
+                }
+                return _instance;
+            }
+        }
+    }
+}
